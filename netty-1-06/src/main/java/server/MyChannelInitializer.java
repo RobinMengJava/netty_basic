1a8df8757dd61d00528569a68dae47a4e5b79d95
robin <RobinMeng2021@qq.com>
2021-11-26 23:08:02 +0800

    用Netty搭建简单的http服务

---
+++ netty-1-06/src/main/java/server/MyChannelInitializer.java
@@ -0,0 +1,28 @@
+package server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.nio.charset.Charset;
+
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+
+        // HttpResponse编码器
+        channel.pipeline().addLast(new HttpResponseEncoder());
+
+        // HttpRequest解码器
+        channel.pipeline().addLast(new HttpRequestDecoder());
+
+        // 消息入站处理器
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+}
---
+++ netty-1-06/src/main/java/server/MyServerHandler.java
@@ -0,0 +1,66 @@
+package server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.EmptyByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.*;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 读取Http请求，即客户端发送来一个Http请求时，调用此方法读取发送过来的请求
+     * */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            DefaultHttpRequest request = (DefaultHttpRequest) msg;
+            System.out.println("URI:" + request.getUri());
+            System.out.println(msg);
+        }
+
+        if (msg instanceof HttpContent) {
+            LastHttpContent httpContent = (LastHttpContent) msg;
+            ByteBuf byteData = httpContent.content();
+            if (!(byteData instanceof EmptyByteBuf)) {
+                // 接收msg消息
+                byte[] msgByte = new byte[byteData.readableBytes()];
+                byteData.readBytes(msgByte);
+                System.out.println(new String(msgByte, Charset.forName("UTF-8")));
+            }
+        }
+
+        String sendMsg = "Hello I'm Robin Meng!";
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,
+                Unpooled.wrappedBuffer(sendMsg.getBytes(Charset.forName("UTF-8"))));
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain;charset=UTF-8");
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+        response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端端断开链接" + ctx.channel().localAddress().toString());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+        System.out.println("异常信息：\r\n" + cause.getMessage());
+    }
+}
