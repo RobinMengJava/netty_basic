f8af91de84ca99494622bc23f19f848a3c3d070a
mengweilu <1079501602@qq.com>
2021-11-27 14:40:45 +0800

    Netty 使用ProtoBuf传递数据

---
+++ netty-2-02/src/main/java/server/MyChannelInitializer.java
@@ -0,0 +1,26 @@
+package server;
+
+import domain.MsgBody;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+
+        // protobuf处理
+        channel.pipeline().addLast(new ProtobufVarint32FrameDecoder());
+        channel.pipeline().addLast(new ProtobufDecoder(MsgBody.getDefaultInstance()));
+        channel.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender());
+        channel.pipeline().addLast(new ProtobufEncoder());
+
+        // 在管道中添加自己接收数据的实现方法
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+}
